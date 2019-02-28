b737e1797e7356d873edfcceb19b6a014f1964ed
xiaolin.huang <xiaolin.huang@iim.ltd>
2019-02-28 17:36:15 +0800

    更新bootload, 解决有时候下载完不能进系统的bug

--- USER/main.c
+++ USER/main.c
@@ -7,12 +7,9 @@
 #include "msg.h"
 #include "iap.h"
 
-#define BOOTLOAD_SHOW_TIME 0x6
 
 int main(void)
-{
-    int max_times = BOOTLOAD_SHOW_TIME;
-    
+{    
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4	 
     delay_init();	    				//延时函数初始化	  
     uart_init(115200);					//初始化串口
@@ -21,39 +18,36 @@ int main(void)
 
     if (IAP_ReadIapFlag() != 0)
     {
-        max_times = 0xffff;
-    }
-    
-    while (1)
-    {
-        for (int i = 0; i < max_times; i++)
-        {
-            printf("Bootload start.\r\n");
-            
-            LED0 = ~LED0;
-            LED1 = ~LED0;
-            delay_ms(500);    
-
-            if (E_IAP_STATE_DOWNLOADING == IAP_GetState())
-            {
-                while(E_IAP_STATE_DOWNLOAD_COMPLETE != IAP_GetState())
-                {
-                    IAP_DownloadFlash();//需要下载最后一包进flash
-                }
-            }
-
-            if (E_IAP_STATE_DOWNLOAD_COMPLETE == IAP_GetState())
-            {
-                IAP_DownloadLastPkgToFlash();
-                IAP_CopyProgramToAppArea();
-                break;
-            }
-        }    
-
         IAP_SetIapFlag(0);
         LOGD("Start app...");
         IAP_LoadApp(APP_START_ADDR);
         IAP_SetState(E_IAP_STATE_NONE);
+    }
+    
+    while (1)
+    {
+        printf("Bootload start.\r\n");
+        
+        LED0 = ~LED0;
+        LED1 = ~LED0;
+        delay_ms(500);    
+
+        if (E_IAP_STATE_DOWNLOADING == IAP_GetState())
+        {
+            while(E_IAP_STATE_DOWNLOAD_COMPLETE != IAP_GetState())
+            {
+                IAP_DownloadFlash();//需要下载最后一包进flash
+            }
+        }
+
+        if (E_IAP_STATE_DOWNLOAD_COMPLETE == IAP_GetState())
+        {
+            IAP_DownloadLastPkgToFlash();
+            IAP_CopyProgramToAppArea();
+            IAP_SetIapFlag(0);
+            IAP_ResetMCU();
+            break;
+        } 
     }        
 }
 
